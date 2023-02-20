161c49e79a56333ea733a8f6b637c17be276ec17
arcogabbo <arcogabbo@gmail.com>
2023-02-20 17:05:03 +0100

    [#IOCIT-348] WIP: activatePubKey handler

--- utils/writers.ts
+++ utils/writers.ts
@@ -3,12 +3,14 @@ import { AssertionRef } from "../generated/definitions/internal/AssertionRef";
 import * as TE from "fp-ts/lib/TaskEither";
 import { CosmosErrors } from "@pagopa/io-functions-commons/dist/src/utils/cosmosdb_model";
 import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
+import { JwkPublicKey } from "@pagopa/ts-commons/lib/jwk";
+import { ActivatedPubKey } from "../generated/definitions/internal/ActivatedPubKey";
 
 // TODO: right: retrievedPopDocument
 // left: errors
 export type PopDocumentWriter = (
   assertionRef: AssertionRef
-) => TE.TaskEither<CosmosErrors, unknown>;
+) => TE.TaskEither<CosmosErrors, ActivatedPubKey>;
 
 // TODO:CHANGE LEFT/RIGHT
 export type AssertionWriter = (
@@ -20,7 +22,7 @@ export type AssertionWriter = (
 export const getPopDocumentWriter = (
   COSMOS_MODEL: any
 ): PopDocumentWriter => assertionRef => {
-  return TE.of({ resource: [] });
+  return TE.of({} as ActivatedPubKey);
 };
 
 export const getAssertionWriter = (BLOB_STORAGE: any): AssertionWriter => (
