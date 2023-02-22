16c0e058cbdb757307bedbe8a76a3ffa90e224dd
arcogabbo <arcogabbo@gmail.com>
2023-02-22 09:18:56 +0100

    WIP: handler error details edit

--- ActivatePubKey/index.ts
+++ ActivatePubKey/index.ts
@@ -3,14 +3,14 @@ import * as express from "express";
 import { secureExpressApp } from "@pagopa/io-functions-commons/dist/src/utils/express";
 import { setAppContext } from "@pagopa/io-functions-commons/dist/src/utils/middlewares/context_middleware";
 import createAzureFunctionHandler from "@pagopa/express-azure-functions/dist/src/createAzureFunctionsHandler";
-import { ActivatePubKey } from "./handler";
+import { createBlobService } from "azure-storage";
 import {
   LolliPOPKeysModel,
   LOLLIPOPKEYS_COLLECTION_NAME
 } from "../model/lollipop_keys";
 import { cosmosdbInstance } from "../utils/cosmosdb";
-import { createBlobService } from "azure-storage";
 import { getConfigOrThrow } from "../utils/config";
+import { ActivatePubKey } from "./handler";
 
 const config = getConfigOrThrow();
 
